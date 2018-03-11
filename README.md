ff2376d58db55712e48bc1ac69007eebaab18667
Mitchel Smith <mitchelsmith527@gmail.com>
2018-03-11 16:37:51 -0500

    Added comments and other improvements.

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 Web application for learning purposes.
+
+Features:
+	- View different campgrounds
+	- Add campgrounds
+	- Review campgrounds
+
+Uses:
+	Languages:
+		- HTML
+		- CSS
+		- JavaScript
+	Databases:
+		- MongoDB
+	Dependencies:
+		- Express
+		- Mongoose
+		- EJS
+		- Body Parser
--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 var express 	= require("express"),
 	bodyParser 	= require("body-parser"),
 	mongoose 	= require("mongoose"),
-	app 		= express();
+	app 		= express(),
+	Campground 	= require("./models/campground"),
+	Comment 	= require("./models/comment"),
+	seedDB		= require("./seeds");
 
+//seedDB();
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-var campgroundSchema = new mongoose.Schema({
-	name: String,
-	image: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
+app.use(express.static(__dirname + "/public"));
 
 // Campground.create(
-// 	{name: "Palo Duro", image: "https://tpwd.texas.gov/state-parks/palo-duro-canyon/gallery/lighthouse-peak.jpg"
+// 	{name: "Palo Duro",
+// 	image: "https://tpwd.texas.gov/state-parks/palo-duro-canyon/gallery/lighthouse-peak.jpg",
+// 	description: "Canyon style park with great trails and camping."
 // 	}, function(err, campground) {
 // 		if(err) {
 // 			console.log(err);
@@ -33,15 +33,16 @@ app.get("/campgrounds", function(req, res) {
 		if(err) {
 			console.log(err);
 		} else {
-			res.render("campgrounds", {campgrounds: campgrounds});
+			res.render("campgrounds/index", {campgrounds: campgrounds});
 		}
 	})
 });
 
 app.post("/campgrounds", function(req, res) {
 	var name = req.body.name;
+	var desc = req.body.description;
 	var image = req.body.image;
-	var newCampground = {name: name, image: image};
+	var newCampground = {name: name, description: desc, image: image};
 
 	Campground.create(newCampground, function(err, newlyCreated) {
 		if(err) {
@@ -53,7 +54,46 @@ app.post("/campgrounds", function(req, res) {
 });
 
 app.get("/campgrounds/new", function(req, res) {
-	res.render("new.ejs");
+	res.render("campgrounds/new.ejs");
+});
+
+app.get("/campgrounds/:id", function(req, res) {
+	Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds/show", {campground: foundCampground});
+		}
+	});
+});
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+	Campground.findById(req.params.id, function(err, campground) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("comments/new", {campground: campground});
+		}
+	});
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+	Campground.findById(req.params.id, function(err, campground) {
+		if(err) {
+			console.log(err);
+			res.redirect("/campgrounds");
+		} else {
+			Comment.create(req.body.comment, function(err, comment) {
+				if(err) {
+					console.log(err);
+				} else {
+					campground.comments.push(comment);
+					campground.save();
+					res.redirect("/campgrounds/" + campground._id);
+				}
+			})
+		}
+	})
 });
 
 app.listen(5000, function() {
