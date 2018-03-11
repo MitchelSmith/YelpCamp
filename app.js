863601442971da5295c0bff8a7f83d707673e54e
Mitchel Smith <mitchelsmith527@gmail.com>
2018-03-11 00:54:17 -0600

    Added start of database and other improvements.

--- app.js
+++ app.js
@@ -1,20 +1,59 @@
-var express = require("express");
-var app = express();
+var express 	= require("express"),
+	bodyParser 	= require("body-parser"),
+	mongoose 	= require("mongoose"),
+	app 		= express();
 
+mongoose.connect("mongodb://localhost/yelp_camp");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+
+var campgroundSchema = new mongoose.Schema({
+	name: String,
+	image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+// 	{name: "Palo Duro", image: "https://tpwd.texas.gov/state-parks/palo-duro-canyon/gallery/lighthouse-peak.jpg"
+// 	}, function(err, campground) {
+// 		if(err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log("New Campground: " + campground);
+// 		}
+// 	});
 
 app.get("/", function(req, res) {
 	res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-	var campgrounds = [
-		{name: "Caprock Canyon", image: "https://tpwd.texas.gov/state-parks/caprock-canyons/gallery/caprock-canyons_7035.jpg"},
-		{name: "Palo Duro", image: "https://tpwd.texas.gov/state-parks/palo-duro-canyon/gallery/lighthouse-peak.jpg"},
-		{name: "Pedernales Falls", image: "https://tpwd.texas.gov/state-parks/pedernales-falls/gallery/spotted-towhee"}
-	]
+	Campground.find({}, function(err, campgrounds) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.render("campgrounds", {campgrounds: campgrounds});
+		}
+	})
+});
 
-	res.render("campgrounds", {campgrounds: campgrounds});
+app.post("/campgrounds", function(req, res) {
+	var name = req.body.name;
+	var image = req.body.image;
+	var newCampground = {name: name, image: image};
+
+	Campground.create(newCampground, function(err, newlyCreated) {
+		if(err) {
+			console.log(err);
+		} else {
+			res.redirect("/campgrounds");
+		}
+	});
+});
+
+app.get("/campgrounds/new", function(req, res) {
+	res.render("new.ejs");
 });
 
 app.listen(5000, function() {
